666e8c750184d9ab3bc31917778c5296a23f8ffb
박찬민 <able3104@gmail.com>
2025-11-24 00:51:19 +0900

    카카오로그인 주석처리함:

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const whitelist = ['http://localhost:3001', 'https://under-phase.com'];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders: '*',
-    methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
+  // const whitelist = ['http://localhost:3001', 'https://under-phase.com'];
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     if (!origin || whitelist.indexOf(origin) !== -1) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   allowedHeaders: '*',
+  //   methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
+  //   credentials: true,
+  // });
   const config = new DocumentBuilder()
     .setTitle('Under Phase API')
     .setDescription('The Under Phase API description')
